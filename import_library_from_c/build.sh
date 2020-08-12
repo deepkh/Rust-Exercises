71ed95e49747bf5dca7e0b429475aad02471735d
deepkh <deepkh@gmail.com>
2020-08-12 11:28:30 +0800

    import_library_from_c: update build.sh

--- import_library_from_c/build.sh
+++ import_library_from_c/build.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
+#############################################################
+# Build: 
+#     Host from linux_x86_64 and target for linux_x86_64
+#         Release   
+#             ./build.sh release
+#         Debug  
+#             ./build.sh dev
+#
+#     Host from linux_x86_64 and target for windows_x86_64
+#     Please install mingw-w64 cross compiler toolchain on linux_x86_64 host machine before build process
+#         Release 
+#            ./build.sh release_mingw_w64
+#         Debug 
+#            ./build.sh dev_mingw_w64
+#
+# Clean:
+#     ./build.sh clean
+#
+# Run:
+#     Host from linux_x86_64
+#          ./build.sh run
+#     Host from windows msys2
+#          ./build.sh run_mingw_w64
+#
+#############################################################
+
+
 CROSSCOMPILER=
 IS_MINGW_W64=
+TARGET_BIN=import_library_from_c
+CLEAN_LIST="target mylib.o libmylib.so libmylib.dll libmylib.dll.a"
 
 build_lib() {
 	${CROSSCOMPILER}gcc mylib.c -O3 -c -fPIC -o mylib.o
 	if [ -z "$IS_MINGW_W64" ]; then
-		echo 1
+		echo ${TARGET_PLATFORM} ${RELEASE}
 		${CROSSCOMPILER}g++ -shared -o libmylib.so mylib.o
 	else
-		echo 2
+		echo ${TARGET_PLATFORM} ${RELEASE}
 		${CROSSCOMPILER}g++ -shared -o libmylib.dll \
 			-Wl,--out-implib=libmylib.dll.a \
 			-Wl,--export-all-symbols \
@@ -32,6 +61,7 @@ _build_mingw_w64() {
 }
 
 dev_mingw_w64() {
+	RELEASE=""
 	_build_mingw_w64
 }
 
@@ -46,6 +76,7 @@ _build_linux_x86_64() {
 }
 
 dev() {
+	RELEASE=""
 	_build_linux_x86_64
 }
 
@@ -54,16 +85,23 @@ release() {
 	_build_linux_x86_64
 }
 
+all() {
+	release
+	dev
+	release_mingw_w64
+	dev_mingw_w64
+}
+
 run() {
-	LD_LIBRARY_PATH=`pwd` ./target/debug/import_library_from_c
+	LD_LIBRARY_PATH=`pwd` ./target/debug/${TARGET_BIN}
 }
 
 run_mingw_w64() {
-	LD_LIBRARY_PATH=`pwd` ./target/x86_64-pc-windows-gnu/debug/import_library_from_c.exe
+	LD_LIBRARY_PATH=`pwd` ./target/x86_64-pc-windows-gnu/debug/${TARGET_BIN}.exe
 }
 
 clean() {
-	rm -rf main target mylib.o libmylib.so libmylib.dll libmylib.dll.a
+	rm -rf ${CLEAN_LIST}
 }
 
 $@
